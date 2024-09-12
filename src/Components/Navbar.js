885db3103b0393c6c0915a1a271532c38cbc4361
Ghanishka <ghanishkaeaswaran25@gmail.com>
2024-09-12 16:21:40 +1000

    update project

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+// Navbar component
+function Navbar() {
+    return (
+      <nav>
+        <h1>Perfumy</h1>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>About us</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    )
+  }
+
+  export default Navbar
---
+++ src/Components/Search.js
@@ -0,0 +1,10 @@
+// Search component
+function Search() {
+    return (
+      <div className="searchbar">
+        <input type="text" placeholder="search" />
+      </div>
+    )
+  }
+
+  export default Search
